0d27f000c2e25f23e3a6b75b20ade75e026961c8
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-26 22:10:56 +0200

    Receiving first websocket connections

--- packages/server/src/app/ws-handler.ts
+++ packages/server/src/app/ws-handler.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage } from 'http';
-import { WebSocket, WebSocketServer, ServerOptions } from 'ws';
+import { WebSocket, WebSocketServer, ServerOptions, RawData } from 'ws';
 
 export class WsHandler {
   private wsServer: WebSocketServer;
@@ -17,5 +17,19 @@ export class WsHandler {
 
   onSocketConnected(socket: WebSocket, request: IncomingMessage) {
     console.log(`New Websocket connections: ${request.url}`);
+    socket.on('message', (data: RawData) => this.onSocketMessage(socket, data));
+    socket.on('close', (code, reason) => this.onSocketClosed(code, reason));
+  }
+
+  onSocketMessage(socket: WebSocket, data: RawData) {
+    const payload = JSON.parse(`${data}`);
+    console.log(`Received message: ${payload}`);
+
+    const reply = JSON.stringify({ reply: 'Message received' });
+    socket.send(reply);
+  }
+
+  onSocketClosed(code: number, reason: Buffer) {
+    console.log(`Socket closed; code=${code}, reason=${reason}`);
   }
 }
