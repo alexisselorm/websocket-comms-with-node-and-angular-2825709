580c643bd7b8666f2d2ac928d00a1d305561d433
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-27 10:22:43 +0200

    setup the chat component

--- packages/types/src/lib/ws-message.ts
+++ packages/types/src/lib/ws-message.ts
@@ -1,13 +1,14 @@
 import { User } from './user';
 
-export type WsMessage = ChatMessage | ChatRelayMesage | SystemNotice;
+export type WsMessage = ChatMessage | ChatRelayMessage | SystemNotice;
 
 export interface ChatMessage {
   event: 'chat';
   contents: string;
 }
 
-export interface ChatRelayMesage {
+export interface ChatRelayMessage {
+  event: 'chatRelay';
   contents: string;
   author: User;
 }
